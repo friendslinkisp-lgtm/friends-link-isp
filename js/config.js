612a4237b71e1cd60842480bc8a447282c59822e
friendslinkisp-lgtm <friendslinkisp@gmail.com>
2026-07-02 22:10:33 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://rhkoeppwrjowpetsgorp.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://rhkoeppwrjowpetsgorp.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoa29lcHB3cmpvd3BldHNnb3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NTYxMTIsImV4cCI6MjA5NzUzMjExMn0.FDLDayYpAYBPMqOF_vs5t3FskZl-s4kginEhTZRUpS0";
 
 const supabaseClient = supabase.createClient(
