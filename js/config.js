fdb17bf5394c6c349e62e4c77f93710735061351
friendslinkisp-lgtm <friendslinkisp@gmail.com>
2026-07-02 22:12:18 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://rhkoeppwrjowpetsgorp.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoa29lcHB3cmpvd3BldHNnb3JwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE5NTYxMTIsImV4cCI6MjA5NzUzMjExMn0.FDLDayYpAYBPMqOF_vs5t3FskZl-s4kginEhTZRUpS0";
+const SUPABASE_KEY = "sb_publishable__V1z0fhGpBWFuZVxTg-4yg_C-2xIvaV";
 
 const supabaseClient = supabase.createClient(
 SUPABASE_URL,
